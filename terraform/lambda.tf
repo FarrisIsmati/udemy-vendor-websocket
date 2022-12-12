f565bc75e1bc72b86da1a487376efe8b3ca753aa
farris.ismati@gmail.com <farris.ismati@gmail.com>
2022-12-12 09:46:16 -0500

    package type

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,12 @@
 resource "aws_lambda_function" "connect" {
   function_name = "${var.app_name}-connect"
+  description   = "Websocket connect store data in dynamodb"
   handler       = "aws_simple_websocket.handler.handler"
   role          = aws_iam_role.lambda_main.arn
   runtime       = "nodejs12.x"
   timeout       = 10
-  image_uri      = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/connect:${var.image_tag}"
+  package_type = "Image"
+  image_uri     = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/connect:${var.image_tag}"
 
   lifecycle {
     ignore_changes = [image_uri]

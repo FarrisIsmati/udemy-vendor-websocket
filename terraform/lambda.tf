d05a32458699c25e50323a23b50de5e77697488f
farris.ismati@gmail.com <farris.ismati@gmail.com>
2022-12-12 09:51:54 -0500

    hail mary

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,11 @@
 resource "aws_lambda_function" "connect" {
+  source = "../.."
   function_name = "${var.app_name}-connect"
   description   = "Websocket connect store data in dynamodb"
   role          = aws_iam_role.lambda_main.arn
   handler       = "aws_simple_websocket.handler.handler"
   image_uri     = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/connect:${var.image_tag}"
-  package_type = "image"
+  package_type = "Image"
   timeout       = 10
 
   # For terraform but shouldn't matter because container is in 16

9385ecb80ddaf56999d17bc550ad11c42da750ab
farris.ismati@gmail.com <farris.ismati@gmail.com>
2022-12-12 09:29:18 -0500

    tester

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,10 +3,10 @@ resource "aws_lambda_function" "connect" {
   handler       = "aws_simple_websocket.handler.handler"
   role          = aws_iam_role.lambda_main.arn
   runtime       = "nodejs16.x"
-  timeout = 10
-  image_uri = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/connect:${var.image_tag}"
+  timeout       = 10
+  filename      = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/connect:${var.image_tag}"
 
   lifecycle {
-    ignore_changes = [image_uri]
+    ignore_changes = [filename]
   }
 }

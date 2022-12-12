fea22a0f3953e1c6679954eaa73cd3c07e43c464
farris.ismati@gmail.com <farris.ismati@gmail.com>
2022-12-12 09:50:02 -0500

    fuck it

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,14 +1,12 @@
 resource "aws_lambda_function" "connect" {
   function_name = "${var.app_name}-connect"
   description   = "Websocket connect store data in dynamodb"
-  handler       = "aws_simple_websocket.handler.handler"
   role          = aws_iam_role.lambda_main.arn
-  runtime       = "nodejs12.x"
-  timeout       = 10
-  package_type = "Image"
+  handler       = "aws_simple_websocket.handler.handler"
   image_uri     = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/connect:${var.image_tag}"
+  package_type = "image"
+  timeout       = 10
 
-  lifecycle {
-    ignore_changes = [image_uri]
-  }
+  # For terraform but shouldn't matter because container is in 16
+  runtime       = "nodejs14.x"
 }

e7985c8e066a27ddd6ee9bc27f99b365a1b92d69
farris.ismati@gmail.com <farris.ismati@gmail.com>
2022-12-12 22:12:54 -0500

    no handle

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,6 @@ resource "aws_lambda_function" "connect" {
   function_name = "${var.app_name}-connect"
   description   = "Websocket connect store data in dynamodb"
   role          = aws_iam_role.lambda_main.arn
-  handler       = "aws_simple_websocket.handler.handler"
   image_uri     = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/connect:${var.image_tag}"
   package_type = "Image"
   timeout       = 10
